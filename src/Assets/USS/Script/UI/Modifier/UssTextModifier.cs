79bdc0dabf528b4e6c73bf3c5fe70c099c55d0fa
Park June Chul <pjc0247@naver.com>
2018-01-08 00:42:42 +0900

    ㅕㅔㅇㅁㅅㄷ

--- src/Assets/USS/Script/UI/Modifier/UssTextModifier.cs
+++ src/Assets/USS/Script/UI/Modifier/UssTextModifier.cs
@@ -13,17 +13,47 @@ public class UssTextModifier
     }
 
     [UssModifierKey("text-align")]
-    public void ApplyTextAlign(Text g, UssValue value)
+    public void ApplyTextAlign(Text g, UssValue[] values)
     {
-        var align = value.AsString();
-
-        if (align == "left")
-            g.alignment = TextAnchor.MiddleLeft;
-        else if (align == "center")
-            g.alignment = TextAnchor.MiddleLeft;
-        else if (align == "right")
-            g.alignment = TextAnchor.MiddleLeft;
+        var alignH = values[0].AsString();
+        if (values.Length == 1)
+        {
+            if (alignH == "left")
+                g.alignment = TextAnchor.MiddleLeft;
+            else if (alignH == "center")
+                g.alignment = TextAnchor.MiddleCenter;
+            else if (alignH == "right")
+                g.alignment = TextAnchor.MiddleRight;
+            else
+                throw new ArgumentException("Invalid param: " + alignH);
+        }
         else
-            throw new ArgumentException("Invalid param: " + align);
+        {
+            var alignV = values[1].AsString();
+
+            if (alignH == "left" && alignV == "top")
+                g.alignment = TextAnchor.UpperLeft;
+            else if (alignH == "center" && alignV == "top")
+                g.alignment = TextAnchor.UpperCenter;
+            else if (alignH == "right" && alignV == "top")
+                g.alignment = TextAnchor.UpperRight;
+
+            else if (alignH == "left" && alignV == "middle")
+                g.alignment = TextAnchor.MiddleLeft;
+            else if (alignH == "center" && alignV == "middle")
+                g.alignment = TextAnchor.MiddleCenter;
+            else if (alignH == "right" && alignV == "middle")
+                g.alignment = TextAnchor.MiddleRight;
+
+            else if (alignH == "left" && alignV == "bottom")
+                g.alignment = TextAnchor.LowerLeft;
+            else if (alignH == "center" && alignV == "bottom")
+                g.alignment = TextAnchor.LowerCenter;
+            else if (alignH == "right" && alignV == "bottom")
+                g.alignment = TextAnchor.LowerRight;
+
+            else
+                throw new ArgumentException("Invalid param: " + alignH + ", " + alignV);
+        }
     }
 }
